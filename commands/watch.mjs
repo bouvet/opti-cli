f90824802e02b8ed82d08772f9f6e5934ae181ea
thorivarnj <thor5674@gmail.com>
2024-12-20 13:29:11 +0100

    feat: make watch command find launchsettings file

--- commands/watch.mjs
+++ commands/watch.mjs
@@ -1,4 +1,5 @@
 import program from '../index.mjs';
+import { exec } from 'child_process'
 
 const cwd = process.cwd();
 
@@ -6,5 +7,46 @@ program
     .command('watch')
     .description('Run dotnet watch with a specific profile from launchsettings')
     .action(async () => {
-        console.log(`Commands are working! Current working directory: ${cwd}`);
+        console.log(`Commands are working! Current working directory: ${cwd}`)
+
+        // Find files
+        const currentDir = process.cwd()
+        const fileName = 'launchSettings.json'
+
+        /*
+         * @constant {array}
+        */
+        const files = await searchFiles(currentDir, fileName)
+
+        if (!files) {
+            console.error(`🤔 Are you sure there is a file named ${fileName} in the current working directory?`)
+        }
+
+        let launchSettings
+
+        if (files.length == 1) {
+            launchSettings = files[0]
+        }
+
+        console.log(launchSettings)
     });
+
+const searchFiles = async (dir, fileName) => {
+    try {
+        const findOutput = await new Promise((resolve, reject) => {
+            exec(`find . -name ${fileName}`, (error, stdout, stderr) => {
+                if (error) {
+                    return reject(`Error: ${error.message}`);
+                }
+                if (stderr) {
+                    return reject(`Stderr: ${stderr}`);
+                }
+                resolve(stdout.split('\n').filter(line => line !== ''));
+            });
+        });
+
+        return findOutput
+    } catch (error) {
+        console.error(error);
+    }
+};
