c4e9813cc2a429d1f312fcca01b1b93c7698e4c9
sonderbjarkum <erdnos@yahoo.no>
2025-01-06 13:28:13 +0100

    Cleanup slpackage command

--- commands/sqlpackage.mjs
+++ commands/sqlpackage.mjs
@@ -1,22 +1,19 @@
 import { runCommand } from '../helpers/commands.mjs';
 import program from '../index.mjs';
 import { exec } from 'node:child_process';
-import { confirm } from '@inquirer/prompts';
 import { Logger } from '../utils/logger.mjs';
 
 const logger = new Logger('sqlpackage');
 const addToPathCommand = `cat << \EOF >> ~/.zprofile
 # Add .NET Core SDK tools
-export PATH="$PATH:/Users/sondre/.dotnet/tools"
+export PATH="$PATH:/Users/$(whoami)/.dotnet/tools"
 EOF`;
 
 program
   .command('sqlpackage')
   .description('Install sqlpackage with dotnet cli')
-  .option('-u, --uninstall', '')
+  .option('-u, --uninstall', 'Uninstalls sqlpackage')
   .action(async ({ uninstall }) => {
-    //dotnet tool install -g microsoft.sqlpackage
-
     if (uninstall) {
       logger.info('Uninstalling sqlpackage...');
       await runCommand(
@@ -37,7 +34,11 @@ program
 
     logger.info('Adding sqlpackage to path...');
 
-    exec(addToPathCommand, (err, stdout, stderr) => {
+    exec(addToPathCommand, (err, stdout, stderror) => {
+      if (stderror) {
+        console.error('stderror:', stderror);
+      }
+
       if (err) {
         console.error(`exec error: ${err}`);
         return;
