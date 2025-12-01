2b43fa32e8cee44c1c960faa98934bf13489d34f
Thor Ivar Nirisen Johansen <thor.johansen@bouvet.no>
2025-12-01 16:01:48 +0100

    build: add commander package correctly with pnpm

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@
 
 global.quit = process.exit;
 
-import { Command } from 'commander';
+import { Command } from "commander"
 import registerCommands from './utils/register-commands.mjs';
 import { Printer } from './utils/printer.mjs';
 
@@ -10,24 +10,24 @@ const printer = new Printer('opti-cli');
 const program = new Command();
 
 (async () => {
-  program
-    .name('opti')
-    .description('Team Optimizely CLI tools.')
-    .version('1.0.0');
+    program
+        .name('opti')
+        .description('Team Optimizely CLI tools.')
+        .version('1.0.0');
 
-  // register all commands in /commands directory
-  await registerCommands();
+    // register all commands in /commands directory
+    await registerCommands();
 
-  program.parse(process.argv);
+    program.parse(process.argv);
 })();
 
 process.on('uncaughtException', (error) => {
-  if (error instanceof Error && error.name === 'ExitPromptError') {
-    printer.info('bye! 👋');
-  } else {
-    // Rethrow unknown errors
-    throw error;
-  }
+    if (error instanceof Error && error.name === 'ExitPromptError') {
+        printer.info('bye! 👋');
+    } else {
+        // Rethrow unknown errors
+        throw error;
+    }
 });
 
 export default program;
