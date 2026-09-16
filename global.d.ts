13fa31dd66b59ea4a3b08668c22ad0f1f7239117
Thor Ivar Nirisen Johansen <thor.johansen@bouvet.no>
2026-09-16 14:40:58 +0200

    Feat/watch default (#30)
    * feat(watch): New options for default profile
    
    * chore: remove .opti folder
    
    ---------
    
    Co-authored-by: sonderbjarkum <erdnos@yahoo.no>

--- global.d.ts
+++ global.d.ts
@@ -1,28 +1,29 @@
 declare global {
-  var quit: typeof process.exit;
+    var quit: typeof process.exit;
 
-  interface ProjectConfig {
-    PROJECT_NAME: string,
-    BACPAC_PATH: string;
-    DB_NAME: string;
-    DB_CONTAINER_NAME: string;
-    DB_PORT: string;
-    CONNECTION_STRING: string;
-    PROJECT_ROOT_PATH: string;
-  }
-
-  namespace NodeJS {
-    interface Process {
-      opti: {
-        projectConfig: ProjectConfig;
-        constants: {
-          defaultDBPort: number
-        };
-      };
+    interface ProjectConfig {
+        PROJECT_NAME: string,
+        BACPAC_PATH: string;
+        DB_NAME: string;
+        DB_CONTAINER_NAME: string;
+        DB_PORT: string;
+        CONNECTION_STRING: string;
+        PROJECT_ROOT_PATH: string;
+        DEFAULT_PROFILE?: string;
     }
-  }
+
+    namespace NodeJS {
+        interface Process {
+            opti: {
+                projectConfig: ProjectConfig;
+                constants: {
+                    defaultDBPort: number
+                };
+            };
+        }
+    }
 }
 
 
 
-export {};
+export { };
